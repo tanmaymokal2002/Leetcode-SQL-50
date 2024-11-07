c6d4cf8c16b47aaa8e6f9e84d84ba2597d25eb25
Tanmay Mokal <108115856+tanmaymokal2002@users.noreply.github.com>
2024-11-08 00:43:47 +0530

    Time: 487 ms (77.96%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,3 +1,12 @@
+-- Approach 1:
+/*
 SELECT name
 FROM Customer
 WHERE referee_id != 2 OR referee_id IS NULL;
+*/
+
+-- Approach 2:
+SELECT name
+FROM Customer
+WHERE COALESCE(referee_id, 0) <> 2; 
+-- coalesce replaces null values to 0 before checking with 2
