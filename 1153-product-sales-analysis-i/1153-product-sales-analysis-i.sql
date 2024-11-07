60cb3ecbe265dd500475dce6d1c18be77d3454eb
Tanmay Mokal <108115856+tanmaymokal2002@users.noreply.github.com>
2024-11-08 02:03:52 +0530

    Time: 1556 ms (27.45%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,8 +1,29 @@
-# Approach 1:
-
+# Approach 1: using JOINS
+/*
 SELECT 
     p.product_name,
     s.year,
     s.price
 FROM Sales AS s
 JOIN Product AS p ON s.product_id = p.product_id
+*/
+
+# Approach 2 : Using Subquery and implicit join
+/*
+SELECT 
+    sub.product_name,
+    s.year,
+    s.price
+FROM Sales s,
+     (SELECT p.product_id, p.product_name FROM Product p) sub
+WHERE sub.product_id = s.product_id
+*/
+
+# Approach 3 : Using Implicit Join(Not preferred)
+
+SELECT 
+    p.product_name,
+    s.year,
+    s.price
+FROM Sales AS s, Product AS p 
+WHERE s.product_id = p.product_id
