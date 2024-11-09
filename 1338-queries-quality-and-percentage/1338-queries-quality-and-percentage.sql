65747011ebeb7c9ab78cd0258828036d1206e345
Tanmay Mokal <108115856+tanmaymokal2002@users.noreply.github.com>
2024-11-09 15:39:07 +0530

    Time: 374 ms (74.81%), Space: 0B (100%) - LeetHub

---
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -0,0 +1,12 @@
+#Approach 1:
+SELECT
+    query_name
+    , ROUND( 
+        SUM(rating/position) / COUNT(query_name)
+    ,2) AS quality
+    , ROUND( 
+        SUM(IF(rating < 3, 1, 0))*100 / COUNT(query_name)
+    ,2) AS poor_query_percentage
+FROM Queries
+WHERE query_name IS NOT NULL
+GROUP BY query_name
