1891f1d2d114af48eec40060d0d09d9b3026ca41
Tanmay Mokal <108115856+tanmaymokal2002@users.noreply.github.com>
2024-11-09 08:57:30 +0530

    Time: 373 ms (64.67%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,4 +1,5 @@
 #Approach 1:
+/*
 SELECT e1.name
 FROM Employee e1
 JOIN (
@@ -8,7 +9,14 @@ JOIN (
 	HAVING count(*) >= 5
 ) e2
 ON e1.id = e2.managerId
+*/
 
+#Approach 2:
+SELECT e1.name
+FROM Employee e1
+JOIN Employee e2 ON e1.id = e2.managerId
+GROUP BY e2.managerId, e1.name
+HAVING count(1) >= 5;
 
 
 
