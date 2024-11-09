c99ae61e15038d3368aa29343924b09f2fc63e96
Tanmay Mokal <108115856+tanmaymokal2002@users.noreply.github.com>
2024-11-09 09:02:34 +0530

    Time: 487 ms (16.67%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,3 +1,5 @@
+#Logic is same but just some minute diff
+
 #Approach 1:
 /*
 SELECT e1.name
@@ -12,12 +14,21 @@ ON e1.id = e2.managerId
 */
 
 #Approach 2:
+/*
 SELECT e1.name
 FROM Employee e1
 JOIN Employee e2 ON e1.id = e2.managerId
 GROUP BY e2.managerId, e1.name
 HAVING count(1) >= 5;
+*/
 
-
-
+#Approach 3:
+SELECT e.name
+FROM Employee e
+WHERE e.id IN (
+SELECT managerId
+FROM Employee 
+GROUP BY managerId
+HAVING COUNT(*) >= 5
+)
 
