1af73f7fd95759bb89ac38e577bbb7f40a8ecd79
Tanmay Mokal <108115856+tanmaymokal2002@users.noreply.github.com>
2024-11-10 12:29:23 +0530

    Time: 449 ms (96.43%), Space: 0B (100%) - LeetHub

--- 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
@@ -3,5 +3,19 @@ SELECT
     , COUNT(DISTINCT user_id) AS active_users
 FROM Activity
 WHERE activity_date 
-BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
+BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27' 
 GROUP BY activity_date
+
+/*
+You can also deal with 30 days filtering as follows:
+
+activity_date > '2019-06-27' AND activity_date <= '2019-07-27'
+
+activity_day BETWEEN '2019-06-28' AND '2019-07-27' (here we manually calculated date prior to 30 days of  '2019-07-27')
+
+DATEDIFF('2019-07-27', activity_date)<30 
+AND 
+DATEDIFF('2019-07-27', activity_date)>=0 
+
+DATEDIFF('2019-07-27', activity_date) BETWEEN 0 AND 29
+*/
