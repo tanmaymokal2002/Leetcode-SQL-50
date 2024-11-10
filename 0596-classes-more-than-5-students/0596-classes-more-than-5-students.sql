b8d58052b351a81eaf8075b249a2d2aec0560a4b
Tanmay Mokal <108115856+tanmaymokal2002@users.noreply.github.com>
2024-11-10 22:42:33 +0530

    Time: 722 ms (5.65%), Space: 0B (100%) - LeetHub

--- 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
+++ 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
@@ -1,6 +1,19 @@
 # Approach 1:
+/*
 SELECT 
     class
 FROM Courses
 GROUP BY class
 HAVING COUNT(1) >= 5
+*/
+
+#Approach 2:
+
+SELECT 
+	DISTINCT class
+FROM 
+	(SELECT 
+		class,
+		COUNT(1) OVER(PARTITION BY class) AS frequency
+	FROM Courses) x
+WHERE frequency >= 5
