7887f714fd9e525ab6026f74fbe6fe070d56b1fd
Tanmay Mokal <108115856+tanmaymokal2002@users.noreply.github.com>
2024-11-16 10:42:18 +0530

    Time: 605 ms (55.7%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,3 +1,5 @@
+#Approach 1: using Union
+/*
 SELECT
     employee_id
     , department_id
@@ -10,6 +12,18 @@ SELECT
 FROM Employee
 GROUP BY employee_id
 HAVINg COUNT(employee_id) = 1
+*/
 
+#Approach 2: using window functions
+WITH cte AS
+(SELECT 
+    *
+    , COUNT(employee_id) OVER(PARTITION BY employee_id) AS Employee_Count
+FROM Employee)
 
+SELECT
+    employee_id
+    , department_id
+FROM cte
+WHERE Employee_Count = 1 OR primary_flag = 'Y'
 
