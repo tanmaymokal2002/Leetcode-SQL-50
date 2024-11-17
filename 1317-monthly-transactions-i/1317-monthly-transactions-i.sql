c18295702045950414a4b77038e3e884a1b24edc
Tanmay Mokal <108115856+tanmaymokal2002@users.noreply.github.com>
2024-11-17 19:00:44 +0530

    Time: 732 ms (32.93%), Space: 0B (100%) - LeetHub

---
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -0,0 +1,9 @@
+SELECT
+    LEFT(trans_date, 7) AS month
+    , country
+    , COUNT(1) AS trans_count
+    , SUM(state = 'approved') AS approved_count
+    , SUM(amount) AS trans_total_amount
+    , SUM((state = 'approved') * amount) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country
