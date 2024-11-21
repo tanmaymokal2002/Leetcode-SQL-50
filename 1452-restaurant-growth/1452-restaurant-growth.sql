89bd1d35b29622a9a3ac1ef050d2993a682504b2
Tanmay Mokal <108115856+tanmaymokal2002@users.noreply.github.com>
2024-11-21 18:38:16 +0530

    Time: 387 ms (60.43%), Space: 0B (100%) - LeetHub

--- 1452-restaurant-growth/1452-restaurant-growth.sql
+++ 1452-restaurant-growth/1452-restaurant-growth.sql
@@ -1,5 +1,6 @@
-# Write your MySQL query statement below
--- Leetcode Solutions:
+
+#Approach 1:
+
 WITH cte AS
 (SELECT visited_on
 	, SUM(amount) AS total_amount
@@ -10,15 +11,9 @@ cte2 AS
 	visited_on
     , SUM(total_amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount
     , ROUND(AVG(total_amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) AS average_amount
-FROM cte
-GROUP BY visited_on)
+FROM cte)
 
 SELECT *
 FROM cte2
-WHERE visited_on >= (
-					SELECT visited_on 
-                    FROM cte2 
-                    ORDER BY visited_on
-                    LIMIT 1
-				   ) + 6
+WHERE visited_on >= ( SELECT MIN(visited_on) FROM cte2 ) + INTERVAL 6 DAY
 ORDER BY visited_on
