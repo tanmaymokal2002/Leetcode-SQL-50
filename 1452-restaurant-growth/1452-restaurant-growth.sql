8a62bc213c1058175e5e006563f3950efd8f0c2c
Tanmay Mokal <108115856+tanmaymokal2002@users.noreply.github.com>
2024-11-21 19:10:52 +0530

    Time: 739 ms (5.62%), Space: 0B (100%) - LeetHub

--- 1452-restaurant-growth/1452-restaurant-growth.sql
+++ 1452-restaurant-growth/1452-restaurant-growth.sql
@@ -1,6 +1,6 @@
 
-#Approach 1:
-
+#Approach 1: using window functions 
+/*
 WITH cte AS
 (SELECT visited_on
 	, SUM(amount) AS total_amount
@@ -17,3 +17,23 @@ SELECT *
 FROM cte2
 WHERE visited_on >= ( SELECT MIN(visited_on) FROM cte2 ) + INTERVAL 6 DAY
 ORDER BY visited_on
+*/
+
+#Approch 2: shorter version and using only subqueries
+
+SELECT visited_on
+	, (
+		SELECT SUM(amount)
+        FROM Customer
+        WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
+    ) AS amount
+    , ROUND((
+		SELECT SUM(amount)/7
+        FROM Customer
+        WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
+    ), 2) AS average_amount
+FROM Customer c
+WHERE visited_on >= (SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY) FROM Customer)
+GROUP BY visited_on
+ORDER BY visited_on
+
