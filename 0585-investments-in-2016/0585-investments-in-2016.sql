c43998eb1ef062f3788ea744ab999b62c4d159db
Tanmay Mokal <108115856+tanmaymokal2002@users.noreply.github.com>
2024-11-22 12:51:35 +0530

    Time: 504 ms (91.61%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,5 +1,8 @@
 
-#Approach 1:
+#Approach 1: your solution
+# you made wrong choices there is no need of cte and group by in outer query
+# learn from your mistakes only right and is not enough have to be optimized as well
+/*
 WITH cte AS
 (SELECT 
 	ROUND(SUM(tiv_2016), 2) AS tiv_2016
@@ -20,3 +23,21 @@ GROUP BY tiv_2015)
 
 SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
 FROM cte
+*/
+
+#Approach 2:
+SELECT 
+	 ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance
+WHERE (lat, lon) IN ( 
+	SELECT lat, lon
+	FROM Insurance
+	GROUP BY lat, lon
+	HAVING COUNT(*) = 1
+    )
+AND tiv_2015 IN (
+	SELECT tiv_2015
+	FROM Insurance
+	GROUP BY tiv_2015
+	HAVING COUNT(*) > 1
+)
