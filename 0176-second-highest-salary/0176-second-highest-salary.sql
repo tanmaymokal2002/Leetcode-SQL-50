ff9ad0bd901281c19284c4d6a89c4e33b33a7afa
Tanmay Mokal <108115856+tanmaymokal2002@users.noreply.github.com>
2024-11-23 11:50:45 +0530

    Time: 288 ms (67.84%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,20 @@
 # Approach 1:
+/*
 SELECT
 	(SELECT
 		DISTINCT salary 
 	FROM EMPLOYEE
 	ORDER BY salary DESC
-	LIMIT 1 OFFSET 1) AS SecondHighestSalary
+	LIMIT 1, 1) AS SecondHighestSalary
+*/
+
+#Approach 2:
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary <> (SELECT MAX(salary) FROM Employee)
+
+
+
+
+
+
